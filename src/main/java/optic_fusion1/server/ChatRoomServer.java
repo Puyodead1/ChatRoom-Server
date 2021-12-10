922207314cbb50f63063b34c2819a3c3fb6ad105
Puyodead1 <14828766+Puyodead1@users.noreply.github.com>
2021-12-10 13:24:28 -0500

    log4j and joptsimple + minor changes
    - Removed TerminalConsoleAppender from log4j config
    + Disabled file logger for development purposes
    + Added Log4J
    + Added JOptSimple
    + Git submodules

--- src/main/java/optic_fusion1/server/ChatRoomServer.java
+++ src/main/java/optic_fusion1/server/ChatRoomServer.java
@@ -6,25 +6,47 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ChatRoomServer {
 
+    private static final Logger LOGGER = LogManager.getLogger(ChatRoomServer.class);
+
     public static void main(String[] args) {
+
+        OptionParser optionParser = new OptionParser();
+        OptionSpec<Void> helpSpec = optionParser.accepts("help").forHelp();
+        OptionSpec<Integer> portSpec = optionParser.accepts("port", "Server port").withOptionalArg().ofType(Integer.class).defaultsTo(8888);
+
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
         try {
-            ServerBootstrap b = new ServerBootstrap();
+            OptionSet optionSet = optionParser.parse(args);
+            if (optionSet.has(helpSpec)) {
+                optionParser.printHelpOn(System.out);
+                return;
+            }
+
+            int serverPort = optionSet.valueOf(portSpec);
+
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ServerChannelInitializer())
                     .option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-            ChannelFuture f = b.bind(8888).sync();
+            ChannelFuture f = b.bind(serverPort);
+            LOGGER.info(String.format("ChatRoom server listening on port %s", serverPort));
+            f.sync();
 
             f.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            LOGGER.fatal(String.format("Failed to start ChatRoom server: %s", e.getLocalizedMessage()));
         } finally {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
